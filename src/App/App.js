ef3d4b50f0b2c3b92aa3b3e989c7dc5796e01a79
Zakaria Nur <s362082@oslomet.no>
2023-02-18 04:38:19 +0100

    v.01
    Navbar done.

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './components/footer';
+import Main from './components/main';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Main />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
