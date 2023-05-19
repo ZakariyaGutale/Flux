23a1b6d42d6ab4882945937e11c163b62aa2b527
ZakariaNu <s362082@oslomet.no>
2023-05-19 17:58:13 +0200

    bug fixes

--- src/App/components/footer.js
+++ src/App/components/footer.js
@@ -2,6 +2,138 @@ import React from 'react';
 
 function Footer() {
   return (
+
+    /*<div>
+      <div className='footer-wrapper'>
+        <div className="footer-basic">
+          <footer>
+            <div className='footer-container'>
+
+              <div class="leaderboard-container">
+                <div class="leaderboard-card">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <div class="leaderboard">
+                    <div class="leaderboard-header">
+                      <h2>Leaderboard</h2>
+                      <svg fill="#000000" width="auto" height="30" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M22,7H16.333V4a1,1,0,0,0-1-1H8.667a1,1,0,0,0-1,1v7H2a1,1,0,0,0-1,1v8a1,1,0,0,0,1,1H22a1,1,0,0,0,1-1V8A1,1,0,0,0,22,7ZM7.667,19H3V13H7.667Zm6.666,0H9.667V5h4.666ZM21,19H16.333V9H21Z" /></svg>
+                    </div>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Rank</th>
+                          <th>Name</th>
+                          <th>Score</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Player 1</td>
+                          <td>300</td>
+                        </tr>
+                        <tr>
+                          <td>2</td>
+                          <td>Player 2</td>
+                          <td>280</td>
+                        </tr>
+                        <tr>
+                          <td>3</td>
+                          <td>Player 3</td>
+                          <td>270</td>
+                        </tr>
+                        <tr>
+                          <td>4</td>
+                          <td>Player 4</td>
+                          <td>260</td>
+                        </tr>
+                        <tr>
+                          <td>5</td>
+                          <td>Player 5</td>
+                          <td>250</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div class="leaderboard-footer">
+                      <h4>Coming Soon ...</h4>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="auto" height="30" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                      </svg>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+               */
+
+              /*<div>
+      <div className='footer-wrapper'>
+        <div className="footer-basic">
+          <footer>
+            <div className='footer-container'>
+
+              <div class="leaderboard-container">
+                <div class="leaderboard-card">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <div class="leaderboard">
+                    <div class="leaderboard-header">
+                      <h2>Leaderboard</h2>
+                      <svg fill="#000000" width="auto" height="30" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M22,7H16.333V4a1,1,0,0,0-1-1H8.667a1,1,0,0,0-1,1v7H2a1,1,0,0,0-1,1v8a1,1,0,0,0,1,1H22a1,1,0,0,0,1-1V8A1,1,0,0,0,22,7ZM7.667,19H3V13H7.667Zm6.666,0H9.667V5h4.666ZM21,19H16.333V9H21Z" /></svg>
+                    </div>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Rank</th>
+                          <th>Name</th>
+                          <th>Score</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Player 1</td>
+                          <td>300</td>
+                        </tr>
+                        <tr>
+                          <td>2</td>
+                          <td>Player 2</td>
+                          <td>280</td>
+                        </tr>
+                        <tr>
+                          <td>3</td>
+                          <td>Player 3</td>
+                          <td>270</td>
+                        </tr>
+                        <tr>
+                          <td>4</td>
+                          <td>Player 4</td>
+                          <td>260</td>
+                        </tr>
+                        <tr>
+                          <td>5</td>
+                          <td>Player 5</td>
+                          <td>250</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div class="leaderboard-footer">
+                      <h4>Coming Soon ...</h4>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="auto" height="30" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                      </svg>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+               */
+
+              
     <div>
       <div className='footer-wrapper'>
         <div className="footer-basic">
